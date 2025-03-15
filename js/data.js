c188b8b898365bf87c52745f2d81e29fbd6dad3b
Andrés Duque <kwideveloper@hotmail.com>
2025-03-15 12:35:40 -0700

    Updated years data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ const languageData = [
             "skills": "Skills <i class='fas fa-graduation-cap'></i>",
             "about": {
                 "h2":"Let me introduce myself <i class='fas fa-user-astronaut'></i>",
-                "p1": "My name is Andrés, I'm 25 years old, I have been a Web developer for <span>5</span> years",
+                "p1": "My name is Andrés, I'm 25 years old, I have been a Web developer for <span>6</span> years",
                 "p2": "Something that has always characterized me is being self-taught, thanks to this I have been able to learn a lot about what I am passionate about, computing. My goal is to provide a service always with good practices, commitment and above all enthusiasm for what I do; I use <span>110%</span> of each job to do an excellent work.",
                 "p3": "Currently i'm taking <span> a professional course of React.js </span>, that im gonna put here in my certificates",
                 "see_certificates": "See certificates <i class='fas fa-award'></i>",
@@ -38,7 +38,7 @@ const languageData = [
             "skills": "Habilidades <i class='fas fa-graduation-cap'></i>",
             "about": {
                 "h2":"Déjame presentarme <i class='fas fa-user-astronaut'></i>",
-                "p1": "Mi nombre es Andrés, tengo 25 años, soy desarrollador Web desde hace <span>5</span> años",
+                "p1": "Mi nombre es Andrés, tengo 25 años, soy desarrollador Web desde hace <span>6</span> años",
                 "p2": "Algo que siempre me ha caracterizado es el ser autodidacta, gracias a ello he podido aprender mucho en lo que me apasiona, la informática. Mi objetivo es brindar un servicio siempre con buenas practicas, empeño y por sobre todo entusiasmo por lo que hago; A cada trabajo le empleo un <span>110%</span> para realizar un trabajo excelente",
                 "p3": "Actualmente, estoy tomando un <span> curso profesional de React.js</span> , que voy a poner aquí en mis certificados",
                 "see_certificates": "Ver certificados <i class='fas fa-award'></i>",
